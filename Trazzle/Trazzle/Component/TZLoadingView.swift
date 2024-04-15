e28fdd2152ac9b89c597ce863f59908784bf4f76
hongzuyeon <hongzuyeon@gmail.com>
2024-04-16 02:30:21 +0900

    Lottie package 추가

---
+++ Trazzle/Trazzle/Component/TZLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  TZLoadingView.swift
+//  Trazzle
+//
+//  Created by 연승주 on 4/16/24.
+//
+
+import SwiftUI
+
+struct TZLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TZLoadingView()
+}
