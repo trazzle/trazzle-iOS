59db500c9db9eb6cc88277f4b686bbea80a27291
joomin <jmnl225@github.com>
2024-04-29 19:29:18 +0900

    여행기록 뷰 생성

---
+++ Trazzle/Trazzle/View/TravelRecordView/TrabelRecordCreateView.swift
@@ -0,0 +1,18 @@
+//
+//  TrabelRecordCreateView.swift
+//  Trazzle
+//
+//  Created by joomin on 4/29/24.
+//
+
+import SwiftUI
+
+struct TrabelRecordCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrabelRecordCreateView()
+}
