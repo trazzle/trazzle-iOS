25c6091aaa81cddc9f7dd4767b7fb8e55b01d6d4
hongzuyeon <hongzuyeon@gmail.com>
2024-05-02 01:13:16 +0900

    로딩뷰 UI 생성, 로딩을 키고 끄는 공통 함수 개발

--- Trazzle/Trazzle/Component/TZLoadingView.swift
+++ Trazzle/Trazzle/Component/TZLoadingView.swift
@@ -2,17 +2,68 @@
 //  TZLoadingView.swift
 //  Trazzle
 //
-//  Created by 연승주 on 4/16/24.
+//  Created by 홍주연 on 4/16/24.
 //
 
-import SwiftUI
+import UIKit
+import Lottie
+import SnapKit
 
-struct TZLoadingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@MainActor
+class TZLoadingView {
+    
+    static let shared = TZLoadingView()
+    private var isShowing = false
+    
+    lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    lazy var animationView: LottieAnimationView = {
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named("loading")
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        animationView.backgroundBehavior = .pauseAndRestore
+        return animationView
+    }()
+    
+    func show() {
+        if isShowing == false {
+            isShowing = true
+            
+            guard let keyWindow = UIApplication.shared.keyWindow else { return }
+            
+            // backgroundView
+            keyWindow.addSubview(backgroundView)
+            backgroundView.snp.makeConstraints {
+                $0.leading.trailing.top.bottom.equalToSuperview()
+            }
+            // animationView
+            animationView.play()
+            backgroundView.addSubview(animationView)
+            animationView.snp.makeConstraints {
+                $0.centerX.centerY.equalToSuperview()
+                $0.width.equalTo(60)
+                $0.height.equalTo(30)
+            }
+        }
     }
-}
-
-#Preview {
-    TZLoadingView()
+    
+    func hide() {
+        if isShowing == true {
+            isShowing = false
+            
+            // backgroundView
+            backgroundView.removeFromSuperview()
+            
+            // animationView
+            animationView.stop()
+            animationView.removeFromSuperview()
+        }
+    }
 }
